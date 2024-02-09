bdcaeac779cb4c73cf41bd0afccd58ead4418d48
Phillip Cui <ronaldphill33@gmail.com>
2024-02-09 11:23:20 +0800

    Setup initial dependencies

---
+++ app/src/main/java/com/rpc/weatherapp/splash/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.rpc.weatherapp.splash
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/rpc/weatherapp/weather/current/CurrentWeatherViewModel.kt
@@ -0,0 +1,13 @@
+package com.rpc.weatherapp.weather.current
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CurrentWeatherViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
